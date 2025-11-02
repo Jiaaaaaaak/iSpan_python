7dc642f05a3e58bcc59bc187d0ccca6b86d31fac
unknown <chiaa0315@gmail.com>
2025-11-02 13:57:31 +0800

    11/1.11/2lesson

--- Module2/2-1.py
+++ Module2/2-1.py
@@ -1,10 +1,16 @@
 # 建立第一支 Python 程式
 
 # 宣告變數
-
-
-
+x = 1
+name = "jia" 
 
 # 輸出變數
+print(x)
+print(name) 
 
 
+# CTRL + S 存檔
+# CTRL + / 註解程式碼
+# CTRL + ` 開啟終端機
+# CTRL + <num> 回到上方視窗
+# ctrl + z 上一動
--- Module3/practice2.py
+++ Module3/practice2.py
@@ -1,11 +1,12 @@
 # 終極密碼 讓使用者能夠重複猜數字，直到猜對為止
 answer = 28
+user_input = 0
 # 告訴使用者需要輸入的數字範圍 input()
-while True:
+while answer != user_input:
     # user_input = input('請輸入數字:')
     # user_input  = int(user_input)
     # # print(type(user_input))
-    user_input = int(put('請輸入數字:'))
+    user_input = int(input('請輸入數字:'))
     
     if user_input <1 or user_input > 80:
         print('超出範圍請重新輸入')
@@ -13,9 +14,9 @@ while True:
          print('請輸入更小的數字')
     elif user_input < answer:
         print('請輸入更大的數字')
-    else:
-        print('恭喜中獎')
-        break
+
+print('恭喜中獎')
+        
 # 超出範圍要顯示「超出範圍請重新輸入」
 # 數字太大 要提示「請輸入更小的數字」
 # 數字太小 要提示「請輸入更大的數字」
